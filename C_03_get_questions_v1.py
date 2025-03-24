8f3362d640a2919ea1881c7b805f40d2e57d21c9
DarkSnac <108771978+DarkSnac@users.noreply.github.com>
2025-03-25 09:59:45 +1300

    Generates only questions

--- C_03_get_questions_v1.py
+++ C_03_get_questions_v1.py
@@ -2,4 +2,40 @@ import csv
 import random
 
 # Retrieve data from the csv file
-file = open("gods.xlsx")
+file = open("gods.csv", 'r')
+
+# Make the data into a list
+all_gods = list(csv.reader(file))
+
+# Remove the row with the headings
+all_gods.pop(0)
+
+# List for questions
+questions = [
+    ["What is - the - of?"],
+    ["Is - a major or minor -?"],
+    ["Who is the deity of -?"],
+    ["Is - a god or goddess?"],
+    ["Is - Greek or Roman?"]
+]
+
+# Main loop
+# while True:
+# Get a random god for the question
+random_god = random.choice(all_gods)
+print(random_god)
+
+# If the god/goddess is nemesis or apollo don't
+# use the last question as its both greek and roman
+
+question = str(random.choice(questions))
+if question == questions[2]:
+    question_replaced = question.replace("-", random_god[4])
+else:
+    question_replaced = question.replace("-", random_god[3], 1)
+    question_replaced = question_replaced.replace("-", random_god[0])
+print(question_replaced)
+
+
+
+
